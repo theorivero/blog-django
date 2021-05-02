fedaf45c5f58893700f5d45e31f4a13067110c3f
theorivero <thrivero1@gmail.com>
2021-05-02 20:07:52 -0300

    add login and register

--- blog_django/settings.py
+++ blog_django/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'theblog'
+    'theblog',
+    'members'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
