64c09346fad27b78e878be9cbfee3f055120720a
theorivero <thrivero1@gmail.com>
2021-05-03 18:32:42 -0300

    Set new post author value to user id

--- theblog/forms.py
+++ theblog/forms.py
@@ -14,7 +14,8 @@ class PostForm(forms.ModelForm):
 		widgets = {
 			'title' : forms.TextInput(attrs={'class': 'form-control'}),
 			'title_tag' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'O texto que aparece na aba do navegador'}),
-			'author' : forms.Select(attrs={'class': 'form-control'}),
+			'author' : forms.TextInput(attrs={'class': 'form-control', 'id': 'author', 'type': 'hidden'}),
+			#'author' : forms.Select(attrs={'class': 'form-control'}),
 			'category' : forms.Select(choices=Category.objects.all().values_list('name', 'name'),attrs={'class': 'form-control'}),
 			'body' : forms.Textarea(attrs={'class': 'form-control'}),
 		}
