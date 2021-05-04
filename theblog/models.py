a0270af68ee5710d0f22c6b60e32866c5edd00ca
theorivero <thrivero1@gmail.com>
2021-05-04 19:55:52 -0300

    add profile pic and social media links

--- theblog/models.py
+++ theblog/models.py
@@ -39,6 +39,12 @@ class Post(models.Model):
 class Profile(models.Model):
 	user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
 	bio = models.TextField()
+	profile_pic = models.ImageField(null=True, blank=True, upload_to="images/profile")
+	website_url = models.CharField(max_length=255,null=True, blank=True)
+	facebook_url = models.CharField(max_length=255,null=True, blank=True)
+	twitter_url = models.CharField(max_length=255,null=True, blank=True)
+	instagram_url = models.CharField(max_length=255,null=True, blank=True)
+
 
 	def __str__(self):
 			return str(self.user)
