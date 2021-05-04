1be877fc869c1898677aac7deadc8932fbf0fdb1
theorivero <thrivero1@gmail.com>
2021-05-04 16:50:52 -0300

    add user profile model

--- theblog/models.py
+++ theblog/models.py
@@ -35,3 +35,10 @@ class Post(models.Model):
 	def get_absolute_url(self):
 		#return reverse('article-detail', args=(str(self.id)))
 		return reverse('home')
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+	bio = models.TextField()
+
+	def __str__(self):
+			return str(self.user)
